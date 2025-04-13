83deee736da5f9878fbd8ead416e32dff4726c5b
KoliaMatiyash <matiyash.klya@gmail.com>
2025-04-13 20:33:14 +0000

    Initial commit

--- git_init_push.sh
+++ git_init_push.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/bash
 if [ "$#" -ne 2 ]; then
 echo "Потрібно вказати два параметри: шлях до каталогу та URL віддаленого репозиторію."
 exit 1
 fi
-dir_path="/home/kolia/Lab2"
+dir_path="$1"
 echo "$dir_path"
 remote_url="$2"
 if [ ! -d "$dir_path" ]; then
@@ -11,9 +11,10 @@ echo "Каталог $dir_path не існує"
 exit 1
 fi
 cd "$dir_path" || exit 1
-git init || (echo "Error: git init failed"; exit 1;)
-git remote add origin "$remote_url" || (echo "Помилка додавання віддаленого репозиторію"; exit 1;)
-git add . || (echo "Помилка додавання файлів"; exit 1;)
-git commit -m "Initial commit" || (echo "Помилка створення коміту"; exit 1;)
-git push -u origin HEAD || (echo "Помилка відправки змін"; exit 1;)
+git init || { echo "Error: git init failed"; exit 1; }
+git remote remove origin 2>/dev/null
+git remote add origin "$remote_url" || { echo "Помилка додавання віддаленого репозиторію"; exit 1; }
+git add . || { echo "Помилка додавання файлів"; exit 1; }
+git commit -m "Initial commit" || { echo "Помилка створення коміту"; exit 1; }
+git push -u origin HEAD || { echo "Помилка відправки змін"; exit 1; }
 echo "Успішно виконано!"
