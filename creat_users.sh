5613a45da5f35bea6320d0de867f9293a6095a34
KoliaMatiyash <matiyash.klya@gmail.com>
2025-04-17 13:03:40 +0000

    Initial commit

--- creat_users.sh
+++ creat_users.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 users=("Nikol" "Jhon" "Vasia")
 for user in "${users[@]}"; do
-# Перевірка існування домашнього каталогу
 if [ -d "/home/$user" ]; then
 echo "Каталог /home/$user вже існує. Пропускаю користувача $user."
 continue
 fi
-# Додавання користувача з автоматичним створенням домашнього каталогу
 if ! useradd -m -s /bin/bash "$user"; then
 echo "Помилка додавання користувача $user!"
 continue
 fi
-# Генерація паролю
 password=$(openssl rand -base64 12 | head -c 16)
 echo "$user:$password" | chpasswd || {
 echo "Помилка встановлення паролю для $user"
 userdel -r "$user" 2>/dev/null
 continue
 }
-# Збереження паролю
 echo "$password" > "/home/$user/${user}_password.txt"
 chown "$user:$user" "/home/$user/${user}_password.txt"
-chmod 600 "/home/$user/${user}_password.txt"
-#Генерація  SSH-ключа
+chmod 755 "/home/$user/${user}_password.txt"
 sudo -u "$user" ssh-keygen -t rsa -b 4096 -f "/home/$user/.ssh/id_rsa" -q -N "" || {
 echo "Помилка генерації SSH-ключа для $user"
 continue
