c803cdcb3edadc2f99d503a2ac0e3791c5d682d0
KoliaMatiyash <162808335+KoliaMatiyash@users.noreply.github.com>
2025-05-21 21:33:35 +0300

    Update creat_users.sh

--- creat_users.sh
+++ creat_users.sh
@@ -11,19 +11,16 @@ echo "Помилка створення користувача $user. Пропу
 continue
 fi
 fi
-password=$(openssl rand -base64 12 | head -c 16)
-if echo "$user:$password" | chpasswd; then
-echo "Пароль встановлено для $user."
 password_file="/home/$user/${user}_password.txt"
-if echo "$password" > "$password_file"; then
-chown "$user:$user" "$password_file"
+if [ ! -f "$password_file" ]; then
+password=$(openssl rand -base64 12 | head -c 16)
+echo "$user:$password" | chpasswd
+echo "$password" > "$password_file"
 chmod 700 "$password_file"
-echo "Пароль збережено у $password_file"
+chown "$user:$user" "$password_file"
+echo "Пароль для $user збережено в $password_file"
 else
-echo "Не вдалося зберегти пароль у файл $password_file"
-fi
-else
-echo "Помилка встановлення паролю для $user."
+echo "Пароль вже існує для $user. Пропускаємо генерацію."
 fi
 ssh_dir="/home/$user/.ssh"
 if [ ! -d "$ssh_dir" ]; then
@@ -36,12 +33,9 @@ echo "Каталог $ssh_dir вже існує."
 fi
 if [ ! -f "$ssh_dir/id_rsa" ]; then
 sudo -u "$user" ssh-keygen -t rsa -b 4096 -f "$ssh_dir/id_rsa" -q -N ""
-cat "$ssh_dir/id_rsa.pub" >> "$ssh_dir/authorized_keys"
-chmod 600 "$ssh_dir/authorized_keys"
-chown "$user:$user" "$ssh_dir/authorized_keys"
 echo "SSH ключі для $user створено."
 else
-echo "SSH ключі для $user вже існують. Пропускаємо."
+echo "SSH ключі для $user вже існують."
 fi
 echo "Користувач $user успішно створений."
 done
